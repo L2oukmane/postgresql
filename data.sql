30e1e21a0e7b473abeed765b7522ac78158145a8
Lucas Shangrita <oustanilokman@gmail.com>
2022-08-12 20:39:09 +0300

    Update data

--- data.sql
+++ data.sql
@@ -5,7 +5,7 @@ VALUES
 ('Gabumon','2018-11-15',2,8,true), 
 ('Pikachu','2021-01-07',1,15.04,false),
 ('devimon','2017-05-12',5,11,true),
-('Charmander','2020-03-08',0,11,false),
+('Charmander','2020-02-08',0,11,false),
 ('Plantmon' , '2021-11-15',2,5.7,true),
 ('Squirtle','1993-04-02',3,12.13,false),
 ('Angemon','2005-06-12',1,45,true),
